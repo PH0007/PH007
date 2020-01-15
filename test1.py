4707d5f26cdcb128099cbc5e87382c5895e46bf0
PH007 <45909992+PH0007@users.noreply.github.com>
2020-01-15 21:05:32 +0800

    简化版
    新建简化版

--- test1.py
+++ test1.py
@@ -1,7 +1,23 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QIcon
 
-def add(a,b):
-    return a+b
 
-if __name__== '__main__':
-    add(10,30)
-    add(50,80)
+class Ui_mainWindow(object):
+    def setupUi(self, mainWindow):
+        mainWindow.setObjectName("mainWindow")
+        mainWindow.setWindowModality(QtCore.Qt.WindowModal)
+        mainWindow.resize(624, 511)
+        # mainWindow.setFixedSize(841,556)
+        QtCore.QMetaObject.connectSlotsByName(mainWindow)
+        mainWindow.setWindowTitle('您好')
+        mainWindow.setWindowIcon(QIcon('logo.png'))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = QtWidgets.QMainWindow()
+    ui = Ui_mainWindow()
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec_())
